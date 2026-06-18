6037dd790bb28dbc6e5bd7c6aefa9f9ac8981031
arthurstelzner@gmail.com <Arthur Iantas Stelzner>
2026-06-18 18:05:16 -0300

    Criar pastas automaticamente

--- index.php
+++ index.php
@@ -3,4 +3,15 @@
     require __DIR__ . '/frontEnd/view/index.html';
     require __DIR__ . '/frontEnd/view/footer.html';
 
+    $diretorioUsuario = 'uploads/usuario';
+    $diretorioAnimal = 'uploads/animais';
+
+
+    if(!is_dir($diretorioUsuario)) {
+        mkdir($diretorioUsuario, 0755, true);
+    }
+    if(!is_dir($diretorioAnimal)) {
+        mkdir($diretorioAnimal, 0755, true);
+    }
+
 ?>
