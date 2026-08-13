553fbf09ac7b0038ffad2c38a97935845d53829c
CARLOS EDUARDO DUHRING <carlos_duhring@estudante.sesisenai.org.br>
2026-08-13 16:17:24 -0300

    remove marcadores de merge quebrados de config.php e index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-﻿<?php
-    $diretorioUsuario = 'uploads/usuario';
-    $diretorioAnimal = 'uploads/animais';
-    
-    if(!is_dir($diretorioUsuario)) {
-        mkdir($diretorioUsuario, 0755, true);
-    }
-    if(!is_dir($diretorioAnimal)) {
-        mkdir($diretorioAnimal, 0755, true);
-    }
-=======
 <?php
 session_start();
 
@@ -19,7 +7,6 @@ $diretorioAnimal = 'uploads/animais';
 if (!is_dir($diretorioUsuario)) {
     mkdir($diretorioUsuario, 0755, true);
 }
->>>>>>> d7c8c99a49bf17e8f8a3af0231d4be6a28ff4bd3
 
 if (!is_dir($diretorioAnimal)) {
     mkdir($diretorioAnimal, 0755, true);
