0e204eebcde033798c08a64924fb56a34e64851c
ARTHUR IANTAS STELZNER <arthur_stelzner@estudante.sesisenai.org.br>
2026-08-20 15:44:07 -0300

    get env no config

--- backEnd/config/config.php
+++ backEnd/config/config.php
@@ -33,9 +33,9 @@
             if(self::$instancia === null) {
                 try {
                     self::$instancia = new PDO(
-                        "mysql:host=localhost;dbname=pet_alliance_db;charset=utf8",
-                        "root",
-                        ""
+                        "mysql:host=" . getenv("DB_HOST") . ";dbname=" . getenv("DB_NAME") . ";charset=utf8",
+                        getenv("DB_USER"),
+                        getenv("DB_SENHA")
                     );
                     self::$instancia->setAttribute(
                         PDO::ATTR_ERRMODE,
