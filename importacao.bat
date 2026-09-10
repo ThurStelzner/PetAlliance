f8d2527cb0f8213373511f9fb9ec699c94a595a0
arthurstelzner@gmail.com <Arthur Iantas Stelzner>
2026-09-10 20:57:40 -0300

    Ajuste na automação

--- importacao.bat
+++ importacao.bat
@@ -6,9 +6,9 @@ git clone https://github.com/ThurStelzner/PetAlliance.git
 
 cd C:\xampp\mysql\bin
 
-start mysqld
+start /b mysqld
 
-timeout 4 /nobreak >nul 
+timeout 5 /nobreak >nul 
 
 set banco="%USERPROFILE%\Documents\PetAlliance\docs\banco.sql"
 
@@ -16,28 +16,30 @@ mysql.exe -u root -P 3306 < %banco%
 
 cd "%USERPROFILE%\Documents\PetAlliance\" 
 
-echo DB_SENHA= >> .env
-echo DB_NAME=pet_alliance_db >> .env
-echo DB_USER=root >> .env
-echo DB_HOST=127.0.0.1:3306 >> .env
-echo ABACATEPAY_API_KEY= >> .env
-echo ABACATEPAY_URL= >> .env
-echo ABACATEPAY_PRODUCT_ID= >> .env
-echo ABACATEPAY_PRODUCT2_ID= >> .env
-echo ABACATEPAY_PRODUCT3_ID= >> .env
-echo ABACATEPAY_PRODUCT4_ID= >> .env
-echo SMTP_HOST= >> .env
-echo SMTP_PORT= >> .env
-echo SMTP_USER= >> .env
-echo SMTP_PASS= >> .env
-echo SMTP_FROM_EMAIL= >> .env
-echo SMTP_FROM_NAME= >> .env
+(
+    echo DB_SENHA= 
+    echo DB_NAME=pet_alliance_db 
+    echo DB_USER=root 
+    echo DB_HOST=127.0.0.1 
+
+    echo ABACATEPAY_API_KEY= 
+    echo ABACATEPAY_URL= 
+    echo ABACATEPAY_PRODUCT_ID= 
+    echo ABACATEPAY_PRODUCT2_ID= 
+    echo ABACATEPAY_PRODUCT3_ID= 
+    echo ABACATEPAY_PRODUCT4_ID= 
+
+    echo SMTP_HOST= 
+    echo SMTP_PORT= 
+    echo SMTP_USER= 
+    echo SMTP_PASS= 
+    echo SMTP_FROM_EMAIL= 
+    echo SMTP_FROM_NAME= 
+) > .env
 
 cd "%USERPROFILE%\Documents\PetAlliance\"
 
 echo Iniciando Local Host
-start "" /MAX https:localhost:9090
-php -S localhost:9090
-
+start "" /MAX http:localhost:9090
 echo Importacao finalizada.
-timeout 5
+C:\xampp\php\php.exe -S localhost:9090
